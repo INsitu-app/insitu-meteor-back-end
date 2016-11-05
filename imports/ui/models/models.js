90bb3088f42cf08ba6ea34660ebc71180cf03006
eskimo___ <insitu.arch.app@gmail.com>
2016-11-05 21:43:03 +0000

    put back

--- imports/ui/models/models.js
+++ imports/ui/models/models.js
@@ -172,7 +172,7 @@ Template.models.events({
         let height = 315;
 
         let page = `?page=viewer&query=${this._id}`;
-        let link = `http://138.197.8.133:3005/${page}`;
+        let link = `http://138.68.144.164:3005/${page}`;
 
         let iframe = `<iframe width="${width}" height="${height}" src="${link}" frameborder="0" allowfullscreen></iframe>`;
 
@@ -196,7 +196,7 @@ Template.models.events({
         let $alert = $(".alert-copy");
 
         let page = `?page=viewer&query=${this._id}`;
-        let link = `http://138.197.8.133:3005/${page}`;
+        let link = `http://138.68.144.164:3005/${page}`;
 
         copyToClipboard(link, (err, status) => {
             $alert
